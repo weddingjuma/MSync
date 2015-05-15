f403b0414eaa5ca05621117817f31df89ccd2d46
fadiga <ibfadiga@gmail.com>
2015-05-15 09:31:45 +0000

    update du sdk-4 vers sdk-21

---
+++ app/build/generated/source/buildConfig/androidTest/debug/ml/jokkolabs/fondasms/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ml.jokkolabs.fondasms.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ml.jokkolabs.fondasms.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
