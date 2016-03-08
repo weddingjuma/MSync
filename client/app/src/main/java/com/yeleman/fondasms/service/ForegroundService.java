8ddf3227ec48a26a58fe74087e00b08c35cb4301
renaud gaudin <rgaudin@gmail.com>
2016-03-08 12:16:49 +0000

    minSdk back to API 10

--- client/app/src/main/java/com/yeleman/fondasms/service/ForegroundService.java
+++ client/app/src/main/java/com/yeleman/fondasms/service/ForegroundService.java
@@ -22,7 +22,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
